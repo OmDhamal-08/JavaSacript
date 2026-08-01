dcb44ea01c50246c8a0ae33896cb0bd52a539353
The_entrepreneur <omdhamal77@gmail.com>
2026-08-01 12:10:02 +0000

    memory access and storing done

--- jsbasics/datatypes_summary.js
+++ jsbasics/datatypes_summary.js
@@ -35,3 +35,34 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// this is the static one so in this we just use stack to store the data 
+let emailId="om@gmail.com"
+
+let anotherEmailId=emailId
+
+anotherEmailId="dhamal@gmail.com"
+
+console.log(anotherEmailId);
+console.log(emailId);
+
+// this is refrence one so in this we just do use heap for the storing of the data in the memory 
+let myobjone=
+{
+    name:"Om",
+    age:21,
+    email:"personal@gmail.com"
+}
+
+let myobjtwo=myobjone
+
+myobjone.email="NOTYOURTYPE@gmail.com"
+myobjtwo.age=22
+
+console.table([myobjone.name,myobjone.age,myobjone.email]);
+console.table([myobjtwo.name,myobjtwo.age,myobjtwo.email]);
+
+
