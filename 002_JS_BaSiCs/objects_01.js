5db031bb05947a8b913d2658783c35ed093f24fc
The_entrepreneur <omdhamal77@gmail.com>
2026-08-13 03:46:34 +0000

    Add comments to clarify symbol usage and object methods in objects_01.js

--- 002_JS_BaSiCs/objects_01.js
+++ 002_JS_BaSiCs/objects_01.js
@@ -15,7 +15,8 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1", // symbol 
+    // this is the deceleration of the symbol it is done like this if not thne it will same as the other fields in the object as a string 
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -27,9 +28,11 @@ const JsUser = {
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
+// if we not access the symbole like this then it will be simple string 
 
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
+// use to lock the object 
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
@@ -37,9 +40,11 @@ JsUser.greeting = function()
 {
     console.log("Hello JS user");
 }
+
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
+// using this we can dirctly access the fields in the object
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
