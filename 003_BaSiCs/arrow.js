287ec996ddf55b30d7a2e4ec6887377d9db52f71
The_entrepreneur <omdhamal77@gmail.com>
2026-09-20 08:24:26 +0000

    Refactor arrow.js to enhance clarity and organization of context explanations

--- 003_BaSiCs/arrow.js
+++ 003_BaSiCs/arrow.js
@@ -4,19 +4,59 @@ const user ={
     Login:function()
     {
         console.log(`${this.username} has logged in`)
-        console.log(this) // this refers to the user object because it is called as a method of the user object
+        console.log(this) 
+        // this refers to the user object because it is called as a method of the user object
     }
 }
 // user.Login()
 // user.username="XYZ"
-// user.Login()// changing the username property of the user object and calling the Login method again to see the updated username
-// console.log(this)// empty becasue this is not in any function or object context, it refers to the global object (window in browsers, global in Node.js)
+// user.Login()
+// changing the username property of the user object and calling the Login method again to see the updated username
+// console.log(this)
+// empty becasue this is not in any function or object context, it refers to the global object (window in browsers, global in Node.js)
 
 // this is mainly use for refers the context of the object in which it is called. It is used to access the properties and methods of the object.
 
 
 function myFunction()
 {
-    console.log(this) // this refers to the global object (window in browsers, global in Node.js) because it is called as a regular function
+    let unsername="JohnDoe"
+    console.log(this.unsername)
+    // this refers to the global object because it is called in the global context, and there is no property named unsername in the global object, so it will return undefined
 }
-myFunction()
+// myFunction()
+
+
+const myArrowFunction= function()
+{
+    let unsername="JohnDoe"
+    console.log(this.unsername)
+    // this refers to the global object because it is called in the global context, and there is no property named unsername in the global object, so it will return undefined
+}
+// myArrowFunction()
+const myArrowFunction2= ()=>
+{
+    let unsername="JohnDoe"
+    console.log(this.unsername)
+}
+myArrowFunction2()
+// this refers to the global object because it is called in the global context, and there is no property named unsername in the global object, so it will return undefined
+
+
+// ()=>() arrow function
+
+// const addtwoNumbers =(a,b) =>
+// {
+//     return a+b
+// }
+
+// const addtwoNumbers =(a,b) => a+b 
+// // implicit return, no need to use return statement and curly braces, it will return the value of the expression after the arrow
+
+// const addtwoNumbers =(a,b) => (a+b)
+// this way is introduced becasue we can return an object literal from an arrow function, if we use curly braces it will be treated as a block of code and not an object literal, so we need to wrap the object literal in parentheses to tell the interpreter that it is an expression and not a block of code.
+
+const addtwoNumbers =(a,b) => ({username: 'JohnDoe'})
+// returning an object literal from an arrow function, we need to wrap the object literal in parentheses to tell the interpreter that it is an expression and not a block of code.
+
+console.log(addtwoNumbers(2,3)) 
